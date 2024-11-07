7e142239287331383857c444d3e6fbb907bf7694
dansbands <dansbands@hotmail.com>
2024-11-07 16:44:33 -0500

    fix bottom nav

--- app/components/layout/bottom-nav.tsx
+++ app/components/layout/bottom-nav.tsx
@@ -1,40 +1,48 @@
-import React from 'react'
+'use client'
+
+import React from "react";
+import Home from "@/app/Media/Icons/Home.svg";
+import Professional from "@/app/Media/Icons/Professional.svg";
+import Portfolio from "@/app/Media/Icons/Portfolio.svg";
+import Contact from "@/app/Media/Icons/Contact.svg";
+import Image from "next/image";
 
 const BottomNav = () => {
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const element = document.getElementById(e.currentTarget.name);
+    element?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <div className="bottom-nav">
       <div className="container">
         <div className="row" id="navbar-example2">
           <ul>
             <li>
-              <a href="#home">
-                Home
-                {/* <img src="@/app/Media/Icons/Home.svg" /> */}
-              </a>
+              <button type="button" name="home" onClick={handleClick}>
+                <Image alt="Home" src={Home} width={40} />
+              </button>
             </li>
             <li>
-              <a href="#professional">
-                Pro
-                {/* <img src="@/app/Media/Icons/Professional.svg" /> */}
-              </a>
+              <button type="button" name="professional" onClick={handleClick}>
+                <Image alt="Professional" src={Professional} width={40} />
+              </button>
             </li>
             <li>
-              <a href="#portfolio">
-                Port
-                {/* <img src="@/app/Media/Icons/Portfolio.svg" /> */}
-              </a>
+              <button type="button" name="portfolio" onClick={handleClick}>
+                <Image alt="Portfolio" src={Portfolio} width={40} />
+              </button>
             </li>
             <li>
-              <a href="#contact">
-                Con
-                {/* <img src="@/app/Media/Icons/Contact.svg" /> */}
-              </a>
+              <button type="button" name="contact" onClick={handleClick}>
+                <Image alt="Contact" src={Contact} width={40} />
+              </button>
             </li>
           </ul>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default BottomNav
+export default BottomNav;
