712036e393e339a630358e4a940b1841f6a5a93d
dansbands <dansbands@hotmail.com>
2024-11-07 13:18:00 -0500

    styling main

--- app/components/home/welcome.tsx
+++ app/components/home/welcome.tsx
@@ -1,17 +1,19 @@
 import React from "react";
+import Favicon2 from "@/app/Media/favicon2.png";
+import Image from "next/image";
 
 const Welcome = () => {
   return (
     <div className="welcome-outer">
       <div className="welcome">
         <div className="container" id="welcome">
-          <img src="Media/Favicon2.png" />
-          <h1>Welcome to my site. </h1>
-          <h1>
+          <Image src={Favicon2} alt="favicon" height={140} width={140} />
+          <p>Welcome to my site. </p>
+          <p>
             I strive to create beautiful interfaces that are intuitive and
             enjoyable to use.
-          </h1>
-          <h2>Feel free to have a look around.</h2>
+          </p>
+          <p>Feel free to have a look around.</p>
           <a className="btn" href="portfolio.html">
             Recent Work
           </a>
