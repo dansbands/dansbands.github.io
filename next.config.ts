d38b207502396ba1c6bdf8dc90f65da80c83b604
dansbands <danodeawebdev@gmail.com>
2025-03-25 22:16:51 -0400

    update nextconfig

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
+  output: "export",
   reactStrictMode: true,
   images: {
     unoptimized: true, // Disable default image optimization
