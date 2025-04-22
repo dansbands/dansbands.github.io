8b96d16cb5056b0684eb20578356b497a33b1d3e
dansbands <danodeawebdev@gmail.com>
2025-04-22 15:08:40 -0400

    collapsible sections on mobile resume

--- app/resume/body.tsx
+++ app/resume/body.tsx
@@ -1,19 +1,34 @@
+"use client";
+
 import { faBriefcase } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 import { experience } from "../util/const";
 import ExperienceItem from "./experience-item";
 
 export default function Body() {
+  const [isExperienceCollapsed, setIsExperienceCollapsed] = useState(true);
+
+  const toggleExperience = () => {
+    setIsExperienceCollapsed(!isExperienceCollapsed);
+  };
+
   return (
     <div className="resume-body">
-      <div className="body-header">
+      <div className="body-header" onClick={toggleExperience}>
         <div className="icon-circle-lg">
           <FontAwesomeIcon icon={faBriefcase} width={20} />
         </div>
         <span>WORK EXPERIENCE</span>
+        <span className="toggle-button">
+          {isExperienceCollapsed ? "+" : "-"}
+        </span>
       </div>
-      <div className="work-experience-list">
+      <div
+        className={`work-experience-list${
+          isExperienceCollapsed ? " collapsed" : ""
+        }`}
+      >
         {experience.map(
           ({ featured, id, company, title, dates, responsibilities }) => (
             <ExperienceItem
--- app/resume/footer-subsection.tsx
+++ app/resume/footer-subsection.tsx
@@ -1,6 +1,8 @@
+"use client";
+
 import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 import ExperienceItem from "./experience-item";
 import Image from "next/image";
 
@@ -19,15 +21,28 @@ const FooterSubsection: React.FC<{
   icons?: Array<{ src: string; height: number; width: number }>;
   otherExperience?: string; // Add the appropriate type for otherExperience
 }> = ({ icon, children, data, icons, otherExperience }) => {
+  const [isExperienceCollapsed, setIsExperienceCollapsed] = useState(true);
+
+  const toggleExperience = () => {
+    setIsExperienceCollapsed(!isExperienceCollapsed);
+  };
+
   return (
     <div className="footer-sub-section">
-      <div className="subsection-header">
+      <div className="subsection-header" onClick={toggleExperience}>
         <div className="icon-circle-md">
           <FontAwesomeIcon icon={icon} width={20} />
         </div>
         <div className="subsection-title">{children}</div>
+        <span className="toggle-button">
+          {isExperienceCollapsed ? "+" : "-"}
+        </span>
       </div>
-      <div className="subsection-body">
+      <div
+        className={`subsection-body${
+          isExperienceCollapsed ? " collapsed" : ""
+        }`}
+      >
         {data?.map(
           ({
             featured,
