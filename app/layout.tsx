ac35ac31e75eacc3eaa22e3a935a55b1fe8f26da
dansbands <danodeawebdev@gmail.com>
2026-04-20 12:17:27 -0400

    restore head and body tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,10 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="32x32" href="/Favicon3.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/Favicon3.png" />
         <link rel="apple-touch-icon" href="/Favicon3.png" />
+      </head>
+      <body>
+        <GlobalNav />
+        {children}
         <GlobalFooter />
         <BuildInfoLogger />
       </body>
